0e56cad427f5cc505612f4be142af0cdad0c8fd3
AtomicJets <jscott@Jamies-MacBook-Air.local>
2014-08-18 18:48:21 +0100

    change a couple of tests

--- test/integration/blockExtractor.js
+++ test/integration/blockExtractor.js
@@ -18,7 +18,7 @@ describe('BlockExtractor', function(){
 
   var be = new BlockExtractor(config.bitcoind.dataDir, config.network);
 
-  var network = config.network === 'testnet' ? networks.testnet: networks.livenet;
+  var network = config.network === 'testnet' ? networks.testnet: networks.startcoin;
 
   it('should glob block files ', function(done) {
     assert(be.files.length>0);
@@ -48,7 +48,7 @@ describe('BlockExtractor', function(){
     be.getNextBlock(function(err,b) {
       assert(!err);
       // 2nd block of testnet3
-      util.formatHashFull(b.hash).should.equal('00000000b873e79784647a6c82962c70d228557d24a747ea4d1b8bbe878e1206');
+      util.formatHashFull(b.hash).should.equal('4d053299de50dea15836958fb19037d6c054632ba8fe3e2e964e9c5dae732fb5');
       assert(b.timestamp > lastTs, 'timestamp > genesis_ts');
       done();
     });
